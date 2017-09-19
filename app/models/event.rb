40315c8f3571d2b86ef8db63cdbb2acdb322e290
Planck1043 <zhao1036713209@gmail.com>
2017-09-19 17:31:19 +0800

    表单单选 UI (固定选项无 Model)

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,20 +1,22 @@
 class Event < ApplicationRecord
+  validates_presence_of :name, :friendly_id
 
- validates_presence_of :name, :friendly_id
+  validates_uniqueness_of :friendly_id
+  validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
 
- validates_uniqueness_of :friendly_id
- validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
+  before_validation :generate_friendly_id, :on => :create
 
- before_validation :generate_friendly_id, :on => :create
+  STATUS = ["draft", "public", "private"]
+  validates_inclusion_of :status, :in => STATUS
 
- def to_param
-   self.friendly_id
- end
+  def to_param
+    self.friendly_id
+  end
 
- protected
+  protected
 
- def generate_friendly_id
-   self.friendly_id ||= SecureRandom.uuid
- end
+  def generate_friendly_id
+    self.friendly_id ||= SecureRandom.uuid
+  end
 
 end
