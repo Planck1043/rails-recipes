7c4f7810ba848445bd04a225db1ab2e39ed115e5
Planck1043 <zhao1036713209@gmail.com>
2017-09-28 20:12:36 +0800

    16-7 回到上一步

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -17,6 +17,20 @@ class RegistrationsController < ApplicationController
     end
   end
 
+  def step1
+    @registration = @event.registrations.find_by_uuid(params[:id])
+  end
+
+  def step1_update
+    @registration = @event.registrations.find_by_uuid(params[:id])
+
+    if @registration.update(registration_params)
+      redirect_to step2_event_registration_path(@event, @registration)
+    else
+      render "step1"
+    end
+  end
+
   def step2
     @registration = @event.registrations.find_by_uuid(params[:id])
   end
@@ -28,6 +42,22 @@ class RegistrationsController < ApplicationController
       redirect_to step3_event_registration_path(@event, @registration)
     else
       render "step2"
+    end
+  end
+
+  def step3
+    @registration = @event.registrations.find_by_uuid(params[:id])
+  end
+
+  def step3_update
+    @registration = @event.registrations.find_by_uuid(params[:id])
+    @registration.status = "confirmed"
+
+    if @registration.update(registration_params)
+      flash[:notice] = "报名成功"
+      redirect_to event_registration_path(@event, @registration)
+    else
+      render "step3"
     end
   end
 
