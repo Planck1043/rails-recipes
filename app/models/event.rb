af01b4300e06b7a72aa6e3d3512a51aef4416751
Planck1043 <zhao1036713209@gmail.com>
2017-09-30 10:00:55 +0800

    18-8 前台的活动状态筛选

--- app/models/event.rb
+++ app/models/event.rb
@@ -13,6 +13,9 @@ class Event < ApplicationRecord
 
   before_validation :generate_friendly_id, :on => :create
 
+  scope :only_public, -> { where( :status => "public" ) }
+  scope :only_available, -> { where( :status => ["public", "private"] ) }
+
   STATUS = ["draft", "public", "private"]
   validates_inclusion_of :status, :in => STATUS
 
